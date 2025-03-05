81a87d07d4020c559942264aa48f83d274918df1
Joykinuthia <joyrosekinuthia@gmail.com>
2025-03-05 19:39:02 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+ console.log(`global scope${customerName}`);
+
+ function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+ }
+ var customerName = 'bob';
+ var bestCustomer;
+
+ function setBestCustomer(){
+    bestCustomer = 'not bob'
+  }
+  
+var customerName = 'bob';
+var bestCustomer;
+
+  function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+  }
+
+  const leastFavoriteCustomer = 'mary';
+  console.log(leastFavoriteCustomer);
+
+  function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'mary'
+  }
